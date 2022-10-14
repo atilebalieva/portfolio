8bc45cf96de61b0358aa21c67f3c5feb3fcb6660
Aisal Tilebalieva <aitilebalieva@Azizs-MacBook-Pro.local>
2022-10-14 15:11:49 -0500

    part-5 of Time interval file was done. New square creates, when before square reaches the bottom

--- tetris_game/JS-part/new.js
+++ tetris_game/JS-part/new.js
@@ -45,146 +45,128 @@ for (let [key, val] of Object.entries(attrs)) {
 newLine (12, verticalLine, 'x1','x2');
 newLine (24, horizontalLine, 'y1','y2');
 
-/* Code to move the square figure*/
 
-let resetButton = document.getElementById('reset-button');
+/* Code to create and move the square figure*/
 
 let squareAttributes = {
    class: 'figure',
    x: 2,
-   y: 2   
+   y: 1   
 };
 
+let currentSquare = null;
+
+let resetButton = document.getElementById('reset-button');
+
 resetButton.addEventListener('click', () => {
-   createNewSquare();
+   onResetButtonClick();
 });
+
+function onResetButtonClick() {
+   createNewSquare();
+   handleTimer();
+   handleKeyDownEventListener();
+};
 
 function randomColor() {
    return '#' + Math.floor(Math.random()*16777215).toString(16);   
-}
+};
 
  function createNewSquare() {
-   let square = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-   setAttributes(square, squareAttributes);
-   square.style.fill =randomColor();
-   playArea.appendChild(square);
+   currentSquare = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+   setAttributes(currentSquare, squareAttributes);
+   currentSquare.style.fill =randomColor();
+   playArea.appendChild(currentSquare);
 
-   let xCoordinateOfSquare = square.getAttribute('x');
-   let yCoordinateOfSquare = square.getAttribute('y');
+};//1
 
-   // let coordinates = {
-   //    x: square.getAttribute('x'),
-   //    y: square.getAttribute('y')
-   // }
-
-
+function handleTimer() {
    setInterval(() => {
-      // if(yCoordinateOfSquare == 2) {
-      //      yCoordinateOfSquare = 2 + 26;
-      //      square.setAttribute('y', yCoordinateOfSquare);
-      //   } else if (yCoordinateOfSquare == 676) {
-      //      return;
-      //   }
-      //   else {
-      //      yCoordinateOfSquare += 27;
-      //      square.setAttribute('y', yCoordinateOfSquare)
-      //   }
-      yCoordinateOfSquare = move1(yCoordinateOfSquare, 'y');
-      
-        
+      moveSquareDown();//3
      }, 1000);
+};//2
+
+function moveSquareDown() {
+   let yCoordinateOfSquare = parseInt(currentSquare.getAttribute('y'));
+
+      if(yCoordinateOfSquare < 676) {
+         yCoordinateOfSquare += 27;
+         currentSquare.setAttribute('y', yCoordinateOfSquare);
+      } else {
+         createNewSquare();
+      }
+};
+
+function handleKeyDownEventListener() {
+   let xCoordinateOfSquare = currentSquare.getAttribute('x');
+   let yCoordinateOfSquare = currentSquare.getAttribute('y');
 
    window.addEventListener('keydown', e =>{
-
       switch(e.code) {
          case 'ArrowDown':
-            console.log('down');
-         if(yCoordinateOfSquare == 2) {
-               log(typeof(yCoordinateOfSquare))
-               yCoordinateOfSquare = 2 + 26;
-               square.setAttribute('y', yCoordinateOfSquare);
-               log(square);
-            } else if (yCoordinateOfSquare == 676) {
-               return;
-            }
-            else {
-               log(yCoordinateOfSquare)
-               yCoordinateOfSquare += 27;
-               log(yCoordinateOfSquare)
-               square.setAttribute('y', yCoordinateOfSquare)
-            }
+            moveSquareDown();//5
             break;
          case 'ArrowLeft':
-            console.log('left');
-            if(xCoordinateOfSquare == 28) {
-               log(xCoordinateOfSquare)
-               xCoordinateOfSquare = 28 - 26;
-               square.setAttribute('x', xCoordinateOfSquare);
-               log(square);
-            } else if (xCoordinateOfSquare == 2) {
-               return;
-            }
-            else {
-               log(xCoordinateOfSquare)
-               xCoordinateOfSquare -= 27;
-               log(xCoordinateOfSquare)
-               square.setAttribute('x', xCoordinateOfSquare)
-            }
-   
+            moveSquareLeft();//6
             break;
          case 'ArrowRight':
-            console.log('Right');
-            if(xCoordinateOfSquare == 2) {
-               log(typeof(xCoordinateOfSquare))
-               xCoordinateOfSquare = 2 + 26;
-               square.setAttribute('x', xCoordinateOfSquare);
-               log(square);
-            } else if (xCoordinateOfSquare == 352) {
-               return;
-            }
-            else {
-               log(xCoordinateOfSquare)
-               xCoordinateOfSquare += 27;
-               log(xCoordinateOfSquare)
-               square.setAttribute('x', xCoordinateOfSquare)
-            }
+            moveSquareRight();//7
             break;
       }
    });
 
+};//4
+
+
+function moveSquareLeft() {
+   let xCoordinateOfSquare = parseInt(currentSquare.getAttribute('x'));
+   if(xCoordinateOfSquare > 2) {
+      xCoordinateOfSquare -= 27;
+      currentSquare.setAttribute('x', xCoordinateOfSquare);
+   }
+
+   // if(xCoordinateOfSquare == 28) {
+   //    xCoordinateOfSquare = 28 - 26; //2
+   //    currentSquare.setAttribute('x', xCoordinateOfSquare);
+   // } else if (xCoordinateOfSquare == 2) {
+   //    return;
+   // }
+   // else {
+   //    xCoordinateOfSquare -= 27;
+   //    currentSquare.setAttribute('x', xCoordinateOfSquare)
+   // }
 };
 
-function move1(coor1, arg1) {
-   if(coor1 == 2) {
-      log(coor1);
-    return  coor1 = 2 + 26;
-   // square.setAttribute(arg1, coor1.value);//28
-   } 
-   else {
-      log(coor1)
-      return   coor1+= 27;
-   // square.setAttribute(arg1, coor1.value)
+function moveSquareRight() {
+
+   let xCoordinateOfSquare = parseInt(currentSquare.getAttribute('x'));
+   if(xCoordinateOfSquare < 352) {
+      xCoordinateOfSquare += 27;
+      currentSquare.setAttribute('x', xCoordinateOfSquare);
    }
-// else if (coor1 == 352) {
-//    return;
-// }
 
-} 
+   // if(xCoordinateOfSquare == 2) {
+   //    log(typeof(xCoordinateOfSquare))
+   //    xCoordinateOfSquare = 2 + 26;
+   //    currentSquare.setAttribute('x', xCoordinateOfSquare);
+   // } else if (xCoordinateOfSquare == 352) {
+   //    return;
+   // }
+   // else {
+   //    xCoordinateOfSquare += 27;
+   //    currentSquare.setAttribute('x', xCoordinateOfSquare)
+   // }
+}
 
-// function move1(coor1, arg1) {
-//    if(coor1.value == 2) {
-//       coor1.value = 2 + 26;
-//    log(coor1.value);
-//    // square.setAttribute(arg1, coor1.value);//28
-//    } 
-//    else {
-//       coor1.value+= 27;
-//       log(coor1.value)
-//    // square.setAttribute(arg1, coor1.value)
-//    }
-// // else if (coor1 == 352) {
-// //    return;
-// // }
-
-// } 
-
+//Summary what we did:
+// Changes in the Tetris game
+// 1. Make the global function currentSquare (it is our square)
+// 2. Make the onResetButtonClick function. There add our new functions:
+// - createNewSquare();
+// - handleTimer();
+// - handleKeyDownEventListener();
+// 3. Inside of the handleTimer we add our setInterval( it is moving square every one sec);
+// 4. HandleKeyDownEventListener is related to keydown;
+// 5. To handle attributes like x and y, inside of the each function(handleKeyDownEventListener, moveSquareLeft, moveSquareRight, moveSquareDown) we add                -let xCoordinateOfSquare = currentSquare.getAttribute('x');
+//       -let yCoordinateOfSquare = currentSquare.getAttribute('y');
+// 6. moveSquareLeft, moveSquareRight, moveSquareDown changed the y attribute from 2 to 1 and changed condition
