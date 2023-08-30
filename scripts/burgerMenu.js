5b07d64f218b0b02b263acf489dce4f7b128458e
atilebalieva <aitilebalieva@gmail.com>
2023-08-30 12:02:44 -0500

    Added mobile-pointer-events and projects links.

---
+++ scripts/burgerMenu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let burgerMenu = document.querySelector('.burger-menu');
+    let menu = document.querySelector('.menu');
+
+    burgerMenu.addEventListener('click', function() {
+      menu.classList.toggle('open');
+
+    });
+  });
